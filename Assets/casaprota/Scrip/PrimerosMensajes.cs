efd56b9a8a95c7c85532242904b50b41b4f5dd73
ChrisXZ23 <110552702+ChrisXZ23@users.noreply.github.com>
2022-10-06 10:59:21 -0600

    casaprota
    inicio

---
+++ Assets/casaprota/Scrip/PrimerosMensajes.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimerosMensajes : mensajes
+{
+    public GameObject Principal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Peticion()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Variablemensaje.SetActive(true);
+            MensajesUI.text = "Agarra el saco con el boton F";
+            Invoke("ResetearTexto", 2f);
+            Principal.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
--- Assets/intefez/scrips/mensajes.cs
+++ Assets/intefez/scrips/mensajes.cs
@@ -8,13 +8,6 @@ public class mensajes : MonoBehaviour
     public GameObject Variablemensaje;
     public Text MensajesUI;
 
-    public virtual void AgarrarSaco()
-    {
-        MensajesUI.text = "Agarra el saco, con el boton F";
-        Invoke("ResetearTexto", 4f);
-        Variablemensaje.SetActive(false);
-    }
-
     public virtual void ResetearTexto()
     {
         MensajesUI.text = "";
