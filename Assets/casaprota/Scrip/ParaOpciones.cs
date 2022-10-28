7f23c54beeffc3ae374e67d3b146807172f493d8
ChrisXZ23 <110552702+ChrisXZ23@users.noreply.github.com>
2022-10-28 12:35:09 -0600

    build
    Menus en varias escenas menos aldea

--- Assets/casaprota/Scrip/ParaOpciones.cs
+++ Assets/casaprota/Scrip/ParaOpciones.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class ParaOpciones : MonoBehaviour
 {
     public GameObject Activasion;
+    public GameObject ApagarAnterior;
 
     public void ActicarOpciones()
     {
         Activasion.SetActive(true);
+        ApagarAnterior.SetActive(false);
+    }
+
+    public void ApagarOpciones()
+    {
+        Activasion.SetActive(false);
+        ApagarAnterior.SetActive(true);
+    }
+
+    public void SolMenuInicial()
+    {
+        Activasion.SetActive(true);
+    }
+
+    public void Apagar()
+    {
+        Activasion.SetActive(false);
     }
 }
